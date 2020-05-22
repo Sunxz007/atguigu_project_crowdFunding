26fdf824f47d25d59163d3373805f648ab507afd
Sunxz007 <977622085@qq.com>
2020-05-23 00:21:12 +0800

    添加日志依赖包

--- suncrowdfunding01-admin-parent/suncrowdfunding02-admin-webui/src/test/java/com/sun/test/CrowdTest.java
+++ suncrowdfunding01-admin-parent/suncrowdfunding02-admin-webui/src/test/java/com/sun/test/CrowdTest.java
@@ -4,6 +4,8 @@ import com.sun.crowd.entity.Admin;
 import com.sun.crowd.mapper.AdminMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -36,5 +38,28 @@ public class CrowdTest {
     public void test() throws SQLException {
         Connection connection = dataSource.getConnection();
         System.out.println(connection);
+    }
+
+    @Test
+    public void testLog(){
+        //获取logger类，传入的class对象就是当前打印日志的类
+        Logger logger = LoggerFactory.getLogger(CrowdTest.class);
+
+        //根据不同的日志级别打印日志
+        logger.debug("Im Debug Level");
+        logger.debug("Im Debug Level");
+        logger.debug("Im Debug Level");
+
+        logger.info("Im Info Level");
+        logger.info("Im Info Level");
+        logger.info("Im Info Level");
+
+        logger.warn("Im Warn Level");
+        logger.warn("Im Warn Level");
+        logger.warn("Im Warn Level");
+
+        logger.error("Im Error Level");
+        logger.error("Im Error Level");
+        logger.error("Im Error Level");
     }
 }

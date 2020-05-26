a71b18d6355a4713030161976b3c8e2e900b061b
Sunxz007 <977622085@qq.com>
2020-05-26 23:33:16 +0800

    AdminHandler 退出登录操作

--- suncrowdfunding01-admin-parent/suncrowdfunding03-admin-component/src/main/java/com/sun/crowd/mvc/handler/AdminHandler.java
+++ suncrowdfunding01-admin-parent/suncrowdfunding03-admin-component/src/main/java/com/sun/crowd/mvc/handler/AdminHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -34,5 +35,12 @@ public class AdminHandler {
         session.setAttribute(CrowdConstant.ATTR_NAME_LOGIN_ADMIN, admin);
         //防止页面留在表单请求路径，刷新后重复提交表单，重定向到另一个地址，更改浏览器地址
         return "redirect:/admin/to/main/page.html";
+    }
+
+    @RequestMapping("/admin/do/loginout.html")
+    public String doLogout(HttpSession session) {
+        // 强制session失效
+        session.invalidate();
+        return "redirect:/admin/to/login/page.html";
     }
 }
